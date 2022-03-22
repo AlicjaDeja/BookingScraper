3ba1a1b798e6c4bd6b272e11056d567ffba6b273
AlicjaDeja <s221405@gmail.com>
2022-03-22 12:58:17 +0100

    2022-03-22 AD

--- scrapper.py
+++ scrapper.py
@@ -1,6 +1,11 @@
 import requests
+from bs4 import BeautifulSoup
 
-url = "https://www.booking.com/hotel/gb/avonmore-london.en-gb.html"
+url = "https://www.booking.com/hotel/gb/avonmore-london.en-gb.html?auth_success=1#tab-reviews"
 response = requests.get(url)
-print(response.status_code)
-print(response.text)
+
+page_dom = BeautifulSoup(response.text, 'html.parser')
+
+reviews = page_dom.select("li.review_list_new_item_block")
+review = reviews.pop()
+print(review)
